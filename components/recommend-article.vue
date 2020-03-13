16741e29ff03d35845092afb316aa765947cd692
Hiroaki Yamada <54619750+vivaruckk17@users.noreply.github.com>
2020-03-13 14:50:13 +0900

    枠を作りました

--- components/recommend-article.vue
+++ components/recommend-article.vue
@@ -1,6 +1,16 @@
 <template>
     <div class="recommend">
-        <p class="recommend-text">人気記事</p>
+        <div class="recommend-text-box">
+            <p class="recommend-text">人気記事</p>
+        </div>
+        <div class="recommend-box">
+            <div class="recommend-img">
+
+            </div>
+            <div class="article">
+                <p>記事を書きましょう</p>
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -9,7 +19,36 @@ export default {
 }
 </script>
 <style>
+.recommend{
+    text-align: center;
+}
+
 .recommend-text{
-    
+    margin: auto;
+    font-size: 1.5rem;
+    width: 15%;
+    color:#404040;
+    border-bottom: solid 2px #707070;
+}
+
+.recommend-text-box{
+    padding: 100px 0 70px 0;
+}
+
+.recommend-box{
+    display: flex;
+    margin: 0 120px 60px 120px;
+
+}
+
+.recommend-img{
+    background-color: #707070;
+    width: 70%;
+    height: 300px;
+}
+
+.article{
+    text-align: center;
+    width: 30%;
 }
 </style>
