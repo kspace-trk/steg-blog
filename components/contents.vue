c4c9b4c1bd798265c92cb1262826a58a701ac2fd
中尾圭吾 <knakao@nakaokeigonoiMac.local>
2020-04-09 05:01:04 +0900

    markdown実装

--- components/contents.vue
+++ components/contents.vue
@@ -1,8 +1,13 @@
 <template>
     <div class="contents-wrapper">
       <div class="contents-title">
-        <h1>{{contents.title}}</h1>
-        </div>
+        <p>{{contents.title}}</p>
+      </div>
+      <div class="contents-img">
+          <img :src="contents.image.url" alt="記事画像"  />
+      </div>
+      <div class="contents-text" v-html="$md.render(contents.contents)">
+      </div>
     </div>
 </template>
 <script>
@@ -16,11 +21,28 @@ export default {
   width: 700px;
   margin: 100px auto 0;
 }
-.contents-title h1{
-  font-size: 30px;
+.contents-title p{
+  font-size: 2rem;
   color: #282828;
   font-family: 'M PLUS Rounded 1c', sans-serif;
   border-bottom:  solid 1px #707070;
   padding-bottom: 5px;
+}
+.contents-img img{
+    width: 100%;
+    margin-bottom: 30px;
+}
+h1{
+    font-size: 2rem;
+    border-bottom: solid 1px #707070;
+    margin-bottom: 1.5rem;
+}
+h2{
+    font-size: 1.5rem;
+    border-bottom: solid 1px #707070;
+    margin-bottom: 1.5rem;
+}
+h3 {
+    font-size: 1.25rem;
 }
 </style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -52,8 +52,17 @@ export default {
   modules: [
     '@nuxtjs/axios',
     'bootstrap-vue/nuxt',
+    '@nuxtjs/markdownit',
   ],
   axios: {
+  },
+  markdownit: {
+    injected: true, // $mdを利用してmarkdownをhtmlにレンダリングする
+    breaks: true, // 改行コードを<br>に変換する
+    html: true, // HTML タグを有効にする
+    linkify: true, // URLに似たテキストをリンクに自動変換する
+    typography: true,  // 言語に依存しないきれいな 置換 + 引用符 を有効にします。
+
   }
 
 }
